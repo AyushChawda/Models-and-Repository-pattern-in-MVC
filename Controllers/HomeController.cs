dce36053806540bb816df202d38fa64fef26faf0
Ayush <ayushchawda123@gmail.com>
2025-06-16 02:27:08 +0530

    create model class

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,15 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        var student = new List<StudentModel>
+        {
+            new StudentModel{rollNo=1,name="Gautam",gander="male",standard=12},
+            new StudentModel{rollNo=2,name="Animesh",gander="male",standard=8},
+            new StudentModel{rollNo=3,name="Vishnu",gander="male",standard=9},
+            new StudentModel{rollNo=4,name="Raghav",gander="male",standard=11},
+            new StudentModel{rollNo=5,name="Surbhi",gander="female",standard=10},
+        };
+        ViewData["student"] = student;
         return View();
     }
 
---
+++ Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository_pattern.Models
+{
+    public class StudentModel
+    {
+        public int rollNo { get; set; }
+        public string name { get; set; }
+        public string gander { get; set; }
+        public int standard { get; set; }
+    }
+}
